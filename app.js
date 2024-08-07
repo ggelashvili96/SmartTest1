588d13359c3201257dbcf47d7b2ab7dda23ab38c
ggelashvili96 <giorgigelashvili1234@gmail.com>
2024-08-07 15:19:51 +0400

    its new work

---
+++ app.js
@@ -0,0 +1,44 @@
+// let a = 87;
+// let b = 65;
+// console.log(a + b);
+
+let student = {
+    firstName: `giorgi`,
+    lastName: `gelashvili`,
+    age: 28,
+    email: `giorgigelashvili1234@gmail.com`,
+    marks: false,
+  };
+  console.log(student.firstName);
+  
+  let m = 45;
+  let n = 23;
+  if (m > n) {
+    console.log(m + n);
+  } else {
+    console.log(n);
+  }
+  
+  let studentName = `giorgi`;
+  if ((studentName = `giorgi`)) {
+    console.log(`Rise and shine ` + studentName);
+  }
+  
+  let bonus;
+  let salary = 2500;
+  if (salary >= 2000) {
+    bonus = 0;
+  } else {
+    bonus = (salary * 10) / 100;
+  }
+  console.log(bonus);
+  
+  let a = 23;
+  let b;
+  let c;
+  b = a / 2;
+  c = a + b;
+  console.log(c);
+  
+  let phone = [`Iphone`, `samsung`, `pixel`, `Huawei`, `xiaomi`];
+  console.log(phone);
