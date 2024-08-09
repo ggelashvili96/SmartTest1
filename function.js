aa0bcfde1f33d76d79b9279d0226563362758db5
ggelashvili96 <giorgigelashvili1234@gmail.com>
2024-08-10 01:55:48 +0400

    ss

---
+++ function.js
@@ -0,0 +1,47 @@
+// function luwi(params) {
+//     for(let i = 0; i<params.length; i++)
+//     { if(i%2==0)
+//         console.log(`luwia`)
+//     }
+    
+// }
+
+
+// console.log(luwi(cifrebi))
+
+
+// function luwi(params) {
+//     for (let i = 0; i < params.length; i++) {
+//         if (i % 2 === 0) {
+//             console.log('luwia');
+//         }else{console.log(i)}
+//     }
+// }
+
+// let cifrebi = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 33, 44, 55, 66, 77];
+
+// // Call the function without logging its result
+// luwi(cifrebi);
+
+// // console.log(luwi(cifrebi))
+
+
+// function price(){
+
+// }
+// let cifrebi=[1,2,3,4,5,6,7,8,9,11,22,33,44,55,66,77]
+
+
+
+
+// const luwi = (params) => {
+//     for (let i = 1; i < params.length; i++) {
+//         if (i % 2 === 0) {
+//             console.log('luwia');
+//         } else {
+//             console.log(i);
+//         }
+//     }
+// };
+
+// luwi(cifrebi)
